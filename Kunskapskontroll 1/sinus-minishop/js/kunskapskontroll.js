dfd6d0322a145012d0fcd9d9e4ea6378bf8336ee
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-11-06 19:31:26 +0100

    Added functions to buttons in main

--- Kunskapskontroll 1/sinus-minishop/js/kunskapskontroll.js
+++ Kunskapskontroll 1/sinus-minishop/js/kunskapskontroll.js
@@ -1,33 +1,67 @@
 
 // making SINUS-logo my resetBTN 
-let reset = document.querySelector('img'); 
-console.log(reset); 
-reset.addEventListener('click', resetAll); 
+let reset = document.querySelector('img');
+reset.addEventListener('click', resetAll);
 
 
-// getting all buttons on side
-let btns = document.querySelectorAll('button');  
-
+/********  PRODUCT BUTTONS ********/
+// getting all buttons
+let btns = document.querySelectorAll('button');
 
 // Changes made when pressing the button on the first product 
 // New picture & name of hoodie
-btns[0].addEventListener('click', function(event) {
-    let img = document.querySelectorAll('img'); 
-        img[2].src = "img/hoodie-forrest.png"; 
+btns[0].addEventListener('click', function () {
+    let img = document.querySelectorAll('img');
+    img[2].src = "img/hoodie-forrest.png";
 
-        let nameOfhoodies = document.querySelectorAll('h3');
-        nameOfhoodies[0].innerText = 'Forrest'; 
-    })
+    let nameOfhoodies = document.querySelectorAll('h3');
+    nameOfhoodies[0].innerText = 'Forrest';
+})
+
+// Make the background on each product random! 
+btns[1].addEventListener('click', function () {
+    let articles = document.querySelectorAll('article');
+    articles[0].style.backgroundColor = makeRandomColor();
+    articles[1].style.backgroundColor = makeRandomColor();
+    articles[2].style.backgroundColor = makeRandomColor();
+})
+
+// Make the "where we are" disappear when you click the third button
+btns[2].addEventListener('click', function() {
+    let footerArticles = document.querySelectorAll('section article');  
+    footerArticles[1].style.display = 'none'; 
+
+})
 
 
+function resetAll() {
 
-    // My reset function
-    function resetAll() {
+    // reset picture
+    let img = document.querySelectorAll('img');
+    img[2].src = "img/hoodie-ash.png";
 
-        // reset picture
-        let img = document.querySelectorAll('img'); 
-        img[2].src = "img/hoodie-ash.png"; 
+    let nameOfhoodies = document.querySelectorAll('h3');
+    nameOfhoodies[0].innerText = 'Ash';
 
-        let nameOfhoodies = document.querySelectorAll('h3');
-        nameOfhoodies[0].innerText = 'Ash'; 
-    }
+    // reset random colors
+    let articles = document.querySelectorAll('article'); 
+    articles[0].style.backgroundColor = 'white'; 
+    articles[1].style.backgroundColor = 'white'; 
+    articles[2].style.backgroundColor = 'white'; 
+
+    // reset "Where we are"
+    let footerArticles = document.querySelectorAll('section article'); 
+    console.log(footerArticles[1]); 
+    footerArticles[1].style.display = 'block'; 
+
+}
+
+
+// function to make random colors! 
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
