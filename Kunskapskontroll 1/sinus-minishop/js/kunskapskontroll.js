fb6d8576d65b08aeaeebe009dd85514417d2341f
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-11-07 15:22:42 +0100

    created list

--- Kunskapskontroll 1/sinus-minishop/js/kunskapskontroll.js
+++ Kunskapskontroll 1/sinus-minishop/js/kunskapskontroll.js
@@ -4,7 +4,28 @@ let reset = document.querySelector('img');
 reset.addEventListener('click', resetAll);
 
 
-/********  PRODUCT BUTTONS ********/
+/********* EVENTLISTENERS ON NAVBAR **********/
+
+// getting all links
+let navlinks = document.querySelectorAll('nav a');
+
+// Eventlistener for my list
+// creating a variable to use to make sure we only make one list
+let onlyOnce = true; 
+
+navlinks[0].addEventListener('mouseover', function() {
+
+    if (onlyOnce) {
+        onlyOnce = false; 
+        createList(); 
+    }
+
+})
+
+
+
+/********  EVENTLISTENERS ON PRODUCT BUTTONS ********/
+
 // getting all buttons
 let btns = document.querySelectorAll('button');
 
@@ -33,6 +54,13 @@ btns[2].addEventListener('click', function() {
 
 })
 
+// Make all buttons in main change to a random color when clicked 
+for(let button of btns) {
+    button.addEventListener('click', function() {
+        button.style.backgroundColor = makeRandomColor(); 
+    })
+}
+
 
 function resetAll() {
 
@@ -54,6 +82,10 @@ function resetAll() {
     console.log(footerArticles[1]); 
     footerArticles[1].style.display = 'block'; 
 
+    // make list disappar 
+    let ul = document.querySelector('ul'); 
+    ul.remove(); 
+    onlyOnce = true; 
 }
 
 
@@ -64,4 +96,32 @@ const makeRandomColor = () => {
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
+}
+
+// function to create a list, that I call on the eventlistener
+
+function createList(){
+    
+    const listItems = ['WISHLIST:', '- Skateboard', '- Longboard', '- Shirt', '- Pants']; 
+    const ul = document.createElement('ul'); 
+    const main = document.querySelector('main'); 
+   
+    let li; 
+    
+    for (let i = 0; i < listItems.length; i++) {
+    li = document.createElement('li'); 
+    let text = document.createTextNode(listItems[i])
+    li.appendChild(text);
+    ul.appendChild(li)
+    li.style.border = '1px solid black'; 
+    li.style.margin = '0 auto'; 
+    li.style.fontSize = '1rem'; 
+    li.style.padding = '0.4rem'; 
+    }
+    
+    main.insertAdjacentElement('afterend', ul);  
+    ul.style.width = '1000px'
+    ul.style.margin = '0 auto'; 
+    ul.style.listStyle = 'none'; 
+
 }
